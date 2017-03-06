e3b6c66b730436862091945103c0afb4a50d280a
Lurker <434848237@qq.com>
2017-03-06 20:26:34 +0800

    修改标题，移除全局样式，更改loading进度条颜色

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,11 +3,11 @@ module.exports = {
   ** Headers of the page
   */
   head: {
-    title: 'starter',
+    title: '引导页',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'Nuxt.js project' }
+      { hid: 'description', name: 'description', content: '描述信息' }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
@@ -16,11 +16,16 @@ module.exports = {
   /*
   ** Global CSS
   */
-  css: ['~assets/css/main.css'],
+  css: [
+    // {
+    //   src:"assets/less/main.less",
+    //   lang:"less-loader"
+    // }
+  ],
   /*
   ** Customize the progress-bar color
   */
-  loading: { color: '#3B8070' },
+  loading: { color: '#28a7e1' },
   /*
   ** Build configuration
   */
