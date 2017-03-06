1440ef6ff8f579ccaf8c6ec0631a9d24c27af777
Lurker <434848237@qq.com>
2017-03-07 00:20:03 +0800

    🌲 change tab size

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,47 +1,59 @@
 module.exports = {
-  /*
-  ** Headers of the page
-  */
-  head: {
-    title: '引导页',
-    meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '描述信息' }
-    ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
-  },
-  /*
-  ** Global CSS
-  */
-  css: [
-    // {
-    //   src:"assets/less/main.less",
-    //   lang:"less-loader"
-    // }
-  ],
-  /*
-  ** Customize the progress-bar color
-  */
-  loading: { color: '#28a7e1' },
-  /*
-  ** Build configuration
-  */
-  build: {
     /*
-    ** Run ESLINT on save
-    */
-    extend (config, ctx) {
-      if (ctx.isClient) {
-        config.module.rules.push({
-          enforce: 'pre',
-          test: /\.(js|vue)$/,
-          loader: 'eslint-loader',
-          exclude: /(node_modules)/
-        })
-      }
+     ** Headers of the page
+     */
+    head: {
+        title: '引导页',
+        meta: [{
+                charset: 'utf-8'
+            },
+            {
+                name: 'viewport',
+                content: 'width=device-width, initial-scale=1'
+            },
+            {
+                hid: 'description',
+                name: 'description',
+                content: '描述信息'
+            }
+        ],
+        link: [{
+            rel: 'icon',
+            type: 'image/x-icon',
+            href: '/favicon.ico'
+        }]
+    },
+    /*
+     ** Global CSS
+     */
+    css: [
+        // {
+        //   src:"assets/less/main.less",
+        //   lang:"less-loader"
+        // }
+    ],
+    /*
+     ** Customize the progress-bar color
+     */
+    loading: {
+        color: '#28a7e1'
+    },
+    /*
+     ** Build configuration
+     */
+    build: {
+        /*
+         ** Run ESLINT on save
+         */
+        extend(config, ctx) {
+            if (ctx.isClient) {
+                config.module.rules.push({
+                    enforce: 'pre',
+                    test: /\.(js|vue)$/,
+                    loader: 'eslint-loader',
+                    exclude: /(node_modules)/
+                })
+            }
+        }
     }
-  }
 }
