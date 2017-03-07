3f33a10356420310218aec376ee7cfe67b65fb84
Lurker <434848237@qq.com>
2017-03-07 10:07:54 +0800

    init

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,44 +1,36 @@
 module.exports = {
-    /*
-     ** Headers of the page
-     */
-    head: {
-        title: '引导页',
-        meta: [{
-                charset: 'utf-8'
-            },
-            {
-                name: 'viewport',
-                content: 'width=device-width, initial-scale=1'
-            },
-            {
-                hid: 'description',
-                name: 'description',
-                content: '描述信息'
-            }
-        ],
-        link: [{
-            rel: 'icon',
-            type: 'image/x-icon',
-            href: '/favicon.ico'
-        }]
-    },
-    /*
-     ** Global CSS
-     */
-    css: [
-        // {
-        //   src:"assets/less/main.less",
-        //   lang:"less-loader"
-        // }
-    ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
-  },
   /*
-  ** Global CSS
-  */
+   ** Headers of the page
+   */
+  head: {
+    title: '引导页',
+    meta: [{
+        charset: 'utf-8'
+      },
+      {
+        name: 'viewport',
+        content: 'width=device-width, initial-scale=1'
+      },
+      {
+        hid: 'description',
+        name: 'description',
+        content: '描述信息'
+      }
+    ],
+    link: [{
+      rel: 'icon',
+      type: 'image/x-icon',
+      href: '/favicon.ico'
+    }]
+  },
+  link: [{
+    rel: 'icon',
+    type: 'image/x-icon',
+    href: '/favicon.ico'
+  }],
+  /*
+   ** Global CSS
+   */
   css: [
     // {
     //   src:"assets/less/main.less",
@@ -47,35 +39,38 @@ module.exports = {
     "~assets/css/pageFadeIn.css"
   ],
   /*
-  ** Customize the progress-bar color
-  */
-  loading: { color: '#28a7e1' },
+   ** Customize the progress-bar color
+   */
+  loading: {
+    color: '#28a7e1'
+  },
   /*
-  ** Build configuration
-  */
+   ** Build configuration
+   */
   build: {
     /*
      ** Customize the progress-bar color
      */
     loading: {
-        color: '#28a7e1'
+      color: '#28a7e1'
     },
     /*
      ** Build configuration
      */
     build: {
-        /*
-         ** Run ESLINT on save
-         */
-        extend(config, ctx) {
-            if (ctx.isClient) {
-                config.module.rules.push({
-                    enforce: 'pre',
-                    test: /\.(js|vue)$/,
-                    loader: 'eslint-loader',
-                    exclude: /(node_modules)/
-                })
-            }
+      /*
+       ** Run ESLINT on save
+       */
+      extend(config, ctx) {
+        if (ctx.isClient) {
+          config.module.rules.push({
+            enforce: 'pre',
+            test: /\.(js|vue)$/,
+            loader: 'eslint-loader',
+            exclude: /(node_modules)/
+          })
         }
+      }
     }
+  }
 }
