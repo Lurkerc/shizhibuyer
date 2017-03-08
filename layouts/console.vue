bfe0930f9092d893038e0d6e041c09bf7ff72d5c
Lurker <434848237@qq.com>
2017-03-08 22:26:56 +0800

    控制台view

---
+++ layouts/console.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <my-header/>
+    <div class="main">
+      <my-nav/>
+      <nuxt/>
+    </div>
+    <my-footer/>
+  </div>
+</template>
+
+<style lang="less">
+  @import '~assets/less/_var.less';
+  .main{width:1200px;margin:0 auto;margin-top:@headerMainHeight;}
+</style>
+
+<script>
+  import MyHeader from '~components/header.vue'
+  import MyFooter from '~components/footer.vue'
+  import MyNav from '~components/seller-nav.vue'
+
+  export default{
+    components: {
+      MyHeader,
+      MyFooter,
+      MyNav
+    }
+  }
+</script>
