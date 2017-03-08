9c9f027d2a3f67f2443816e48a1f3388a8eb04b2
Lurker <434848237@qq.com>
2017-03-08 13:27:49 +0800

    init

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,75 +1,41 @@
 module.exports = {
   /*
-   ** Headers of the page
-   */
+  ** Headers of the page
+  */
   head: {
-    title: '引导页',
-    meta: [{
-        charset: 'utf-8'
-      },
-      {
-        name: 'viewport',
-        content: 'width=device-width, initial-scale=1'
-      },
-      {
-        hid: 'description',
-        name: 'description',
-        content: '描述信息'
-      }
+    title: 'starter',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: 'Nuxt.js project' }
     ],
-    link: [{
-      rel: 'icon',
-      type: 'image/x-icon',
-      href: '/favicon.ico'
-    }]
-  },
-  link: [{
-    rel: 'icon',
-    type: 'image/x-icon',
-    href: '/favicon.ico'
-  }],
-  /*
-   ** Global CSS
-   */
-  css: [
-    // {
-    //   src:"assets/less/main.less",
-    //   lang:"less-loader"
-    // }
-    "~assets/css/pageFadeIn.css"
-  ],
-  /*
-   ** Customize the progress-bar color
-   */
-  loading: {
-    color: '#28a7e1'
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
   },
   /*
-   ** Build configuration
-   */
+  ** Global CSS
+  */
+  css: ['~assets/css/pageFadeIn.css'],
+  /*
+  ** Customize the progress-bar color
+  */
+  loading: { color: '#41bdf7' },
+  /*
+  ** Build configuration
+  */
   build: {
     /*
-     ** Customize the progress-bar color
-     */
-    loading: {
-      color: '#28a7e1'
-    },
-    /*
-     ** Build configuration
-     */
-    build: {
-      /*
-       ** Run ESLINT on save
-       */
-      extend(config, ctx) {
-        if (ctx.isClient) {
-          config.module.rules.push({
-            enforce: 'pre',
-            test: /\.(js|vue)$/,
-            loader: 'eslint-loader',
-            exclude: /(node_modules)/
-          })
-        }
+    ** Run ESLINT on save
+    */
+    extend (config, ctx) {
+      if (ctx.isClient) {
+        config.module.rules.push({
+          enforce: 'pre',
+          test: /\.(js|vue)$/,
+          loader: 'eslint-loader',
+          exclude: /(node_modules)/
+        })
       }
     }
   }
