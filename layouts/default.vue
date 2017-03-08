401eaa57c3dd4c67f7570f5b4cdad58f2509d219
Lurker <434848237@qq.com>
2017-03-08 13:28:53 +0800

    移除无用样式

--- layouts/default.vue
+++ layouts/default.vue
@@ -6,9 +6,14 @@
   </div>
 </template>
 
+<style lang="less">
+  @import '~assets/less/_var.less';
+  .main{width:1200px;margin:0 auto;margin-top:@headerMainHeight;}
+</style>
+
 <script>
-import MyFooter from '~components/footer.vue';
-import MyHeader from '~components/header.vue';
+import MyFooter from '~components/footer.vue'
+import MyHeader from '~components/header.vue'
 
 export default {
   components: {
@@ -17,39 +22,3 @@ export default {
   }
 }
 </script>
-
-<style>
-.container
-{
-  margin: 0;
-  width: 100%;
-  padding: 100px 0;
-  text-align: center;
-}
-
-.button, .button:visited
-{
-  display: inline-block;
-  color: #3B8070;
-  letter-spacing: 1px;
-  background-color: #fff;
-  border: 2px solid #3B8070;
-  text-decoration: none;
-  text-transform: uppercase;
-  padding: 15px 45px;
-}
-
-.button:hover, .button:focus
-{
-  color: #fff;
-  background-color: #3B8070;
-}
-
-.title
-{
-  color: #505153;
-  font-weight: 300;
-  font-size: 2.5em;
-  margin: 0;
-}
-</style>
